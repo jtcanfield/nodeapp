5b95c06fca07224829d97e2751a323dce7f25fd4
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-15 12:56:05 -0400

    connected html

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-console.log("Server active on http://localhost:3000/");
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+const port = 3000;
+const path = require('path');
 
-app.listen(3000);
+app.use('/nodeapp', express.static(__dirname + '/nodeapp'));
+
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+  // app.use('/index.html', express.static('public'))
+})
+
+
+app.listen(port, function(){
+  console.log("Server active on http://localhost:3000/");
+});
