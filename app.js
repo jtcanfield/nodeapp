71c60da8d37f8433f76bbc1c06e948aa2a6105cd
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-15 11:34:07 -0400

    started server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-console.log("why.... please.... save me");
+console.log("Server active on http://localhost:3000/");
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-// app.listen(3000);
+app.listen(3000);
