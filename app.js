369a20090bded373f2c96cc8ef6a49594fa99059
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-15 14:09:49 -0400

    add working mustache

--- app.js
+++ app.js
@@ -2,29 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const mustache = require('mustache');
+// const mustache = require('mustache');
 const mustacheExpress = require('mustache-express');
 
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
-
+app.use('/nodeapp', express.static(__dirname + '/nodeapp'));
 app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
 app.set('views', './views')
+// app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
   res.render('index', { userName: 'Sam' })
 })
 
-
-
-
-
-
-
-
-
-app.use('/nodeapp', express.static(__dirname + '/nodeapp'));
 app.get("/", function(req, res){
   res.sendFile(path.join(__dirname + '/index.mustache'));
 });
